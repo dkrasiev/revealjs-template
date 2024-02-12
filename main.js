5335b216fe1b31921d6a3510570eb5b2186b533a
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-02-12 12:41:09 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import 'reveal.js/dist/reset.css'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
+
+import Reveal from 'reveal.js'
+
+const app = new Reveal({
+  hash: true,
+  slideNumber: true,
+})
+
+app.initialize()
