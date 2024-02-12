a3f9521c48a18a6a0fe8caba568d4c9433ebf525
Dmitry Krasiev <dmitriy.krasiev@gost-group.com>
2024-02-12 17:55:21 +0400

    feat: add highlight plugin

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ import 'reveal.js/dist/reset.css'
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/black.css'
 
+// highlight
+import 'reveal.js/plugin/highlight/monokai.css'
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
+
 import Reveal from 'reveal.js'
 
 const app = new Reveal({
   hash: true,
-  slideNumber: true,
+  plugins: [RevealHighlight],
 })
 
 app.initialize()
